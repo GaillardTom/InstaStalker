e7aa6d8f80b0627180fdd7c9d9e100a0651ae53f
Tom Gaillard <LE1826622@crc-lennox.qc.ca>
2024-04-07 02:58:11 -0400

    Still needs to figure out how to regenerate the token autonomously and also maybe make a website that can show the data easily

---
+++ README.md
@@ -0,0 +1,8 @@
+# Instagram Stalker for myself 
+
+
+[input] "targets.txt" contains 1 username for target with each line
+
+[How To use it ?] get cookies from burpsuite and change them at the top of the main.py file the targets also have to be public
+
+[Output] "data/{name}_{curr_date}" with every following that the user has 
---
+++ database.py
@@ -0,0 +1,40 @@
+from pymongo import MongoClient
+from dotenv import load_dotenv
+import os
+# import ssl
+load_dotenv()
+
+
+def ConnectToDB(collName):
+    CONNECTION_STRING = os.environ.get("CONNECTION_STRING")
+    client = MongoClient(CONNECTION_STRING)
+    mydb = client['Discord-CompanionDB']
+    collection = mydb[collName]
+    return collection
+
+
+def fetch(collName: str):
+    coll = ConnectToDB(collName)
+    document = list(coll.find({}))
+    return document, coll
+
+
+def InsertScan(scan: dict, victimUsername: str):
+    doc, coll = fetch(victimUsername)
+    result = coll.insert_many(scan)
+    return result
+
+
+def InsertUserIfNotFound(collection, user, datetime_inserted):
+    # collection.find_one({"pk": user["pk"]})
+    # print(user)
+    # print(isFound)
+    if(collection.find_one({"pk": user["pk"]}) is None):
+        user['datetime_inserted'] = datetime_inserted
+        resp = collection.insert_one(user)
+        if(resp.inserted_id):
+            print(f"username: {user['username']}")
+            return True
+        else:
+            print(f"FAILED TO INSERT DOCUMENT: {user}")
+            return False
